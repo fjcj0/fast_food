7bcb11b910066a583ad71baf52b59d16e3ab961a
omar <omarqais225@gmail.com>
2025-07-13 01:14:54 +0300

    set up collection user

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform: "com.jsm.foodordering",
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: process.env.EXPO_PUBLIC_DATABASEID,
+    userCollectionId: process.env.EXPO_PUBLIC_USERCOLLECTIONID,
 }
